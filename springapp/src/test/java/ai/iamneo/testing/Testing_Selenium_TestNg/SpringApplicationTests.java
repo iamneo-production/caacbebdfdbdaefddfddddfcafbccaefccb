4165c4925f93e1ca4e2741f876a62cdc0eb09248
Iamneo <techadmin@iamneo.ai>
2023-08-05 06:43:57 +0000

    Backup Commit

--- springapp/src/test/java/ai/iamneo/testing/Testing_Selenium_TestNg/SpringApplicationTests.java
+++ springapp/src/test/java/ai/iamneo/testing/Testing_Selenium_TestNg/SpringApplicationTests.java
@@ -1,4 +1,4 @@
-package com.examly.springapp;
+/*package com.examly.springapp;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -81,5 +81,45 @@ public class SpringApplicationTests {
 			
 	}
 
+
+}
+*/
+package ai.iamneo.testing.Testing_Selenium_TestNg;
+
+public class AppTest {
+	ChromeDriver driver;
+	String url = "http://www.flipkart.com";
+
+	public void invokeBrowser() {
+
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\Saurabh Dhingra\\workspace\\libs\\chromeDriver36\\chromedriver.exe");
+
+		driver = new ChromeDriver();
+
+		driver.manage().window().maximize();
+
+		driver.manage().deleteAllCookies();
+
+		driver.manage().timeouts().pageLoadTimeout(90, TimeUnit.SECONDS);
+
+		driver.manage().timeouts().implicitlyWait(6, TimeUnit.SECONDS);
+
+		driver.get(url);
+
+	}
+
+	public void loginToFlipkart() {
+		driver.findElement(By.xpath("//div[@class='_1XBjg- row']//form//input[@type='text']")).sendKeys("testuser@abc.com");
+		
+		driver.findElement(By.xpath("//div[@class='_1XBjg- row']//form//input[@type='password']")).sendKeys("Test@1234");
+		
+		driver.findElement(By.xpath("//div[@class='_1XBjg- row']//form//button[@type='submit']")).click();
+		
+	}
+	
+	public void closeBrowser() {
+		driver.quit();
+	}
 
 }
